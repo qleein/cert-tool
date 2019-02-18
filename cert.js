d98490fead4426280b2ea49eccd551a0c4edd7ec
qlee <i@qlee.in>
2019-02-18 22:31:55 +0800

    remove useless code

--- cert.js
+++ cert.js
@@ -1,14 +1,9 @@
-let certificateBuffer = new ArrayBuffer(0); // ArrayBuffer with loaded or created CERT
-let privateKeyBuffer = new ArrayBuffer(0);
-let hashAlg = "SHA-256";
-let signAlg = "RSASSA-PKCS1-v1_5";
 
 let CACertificateInfo = {
     type: "self-signed",
     upload: {},
     created: {},
 };
-
 
 function checkCACertificate() {
     var ty = CACertificateInfo.type;
@@ -53,7 +48,6 @@ function copyToClipboard(id) {
         }
     }
 }
-
 
 function handleWhereIsCA(evt) {
     var radio = document.getElementsByName("whereisca");
@@ -168,50 +162,6 @@ function createCACert()
         console.log("called");
         wasmCreateCACertificate(buf, certinfo, setCACertContent);       
     });
-
-/*
-    const certificate = new Certificate();
-
-	certificate.version = 2;
-    setSerialNumber(certificate, Date.now());
-    setEntity(certificate.issuer, names)
-    setEntity(certificate.subject, names);
-    setValidityPeriod(certificate, new Date(), 3650);
-	certificate.extensions = []; // Extensions are not a part of certificate by default, it's an optional array
-    setCABit(certificate, true, 2);
-    setKeyUsage(certificate, false, false, false, false, false, true, false);
-
-    var privateKey;
-    const crypto = getCrypto();
-    if (typeof crypto === "undefined")
-        return Promise.reject("No WebCrypto extension found");
-
-    return createCertificateFinal(certificate, null, "RSASSA-PKCS1-v1_5", "SHA-256").then(result =>
-    {
-        privateKey = result.privateKey;
-        CACertificateInfo["created"].cert = certificate;
-        CACertificateInfo["created"].subject = certificate.subject;
-        CACertificateInfo["created"].privateKey = privateKey;
-    }) .
-    then(() =>
-    {
-        var pemCert = convertBinaryToPem(certificate.toSchema(true).toBER(false), "CERTIFICATE");
-        document.getElementById("ca-cert-pem").textContent = pemCert;
-        var pemUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(pemCert);
-        document.getElementById("ca-certificate-download").setAttribute("href", pemUrl);
-    }) .
-    then(() =>
-    {
-        crypto.exportKey("pkcs8", privateKey).
-        then(pkcs8 =>
-        {
-            var pemPrivateKey = convertBinaryToPem(pkcs8, "PRIVATE KEY");
-            document.getElementById("ca-private-key-pem").textContent = pemPrivateKey;
-            var pemUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(pemPrivateKey);
-            document.getElementById("ca-private-key-download").setAttribute("href", pemUrl);
-        });
-    });
-*/
 }
 
 
@@ -288,311 +238,22 @@ function createCert() {
             wasmCreateCertificate(buf, certinfo, undefined, setCertContent);
         }
     });
+}
 
-/*
-    var CACert = checkCACertificate();
-    if (!CACert) {
-        console.log("Not ca certificate defined");
-        return;
+
+function setPEMContent(cert, pkey) {
+    if (typeof cert != "undefined") {
+        pemCertUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(cert)
+        document.getElementById("certificate-pem").textContent = cert
+        document.getElementById("certificate-download").setAttribute("href", pemCertUrl)
     }
 
-    const certificate = new Certificate();
-    let privateKey;
-    const crypto = getCrypto();
-    if (typeof crypto === "undefined")
-        return Promise.reject("No WebCrypto extension found");
-
-	certificate.version = 2;
-    setSerialNumber(certificate, Date.now());
-
-    if (CACert.subject) {
-        certificate.issuer = CACert.subject;
-    } else {
-        setEntity(certificate.issuer, names)
+    if (typeof pkey != "undefined") {
+        pemPkeyUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(pkey)
+        document.getElementById("private-key-pem").textContent = pkey
+        document.getElementById("private-key-download").setAttribute("href", pemPkeyUrl)
     }
-
-    setEntity(certificate.subject, names);
-    setValidityPeriod(certificate, new Date(), 3650);
-	certificate.extensions = []; // Extensions are not a part of certificate by default, it's an optional array
-    //setCABit(certificate, true, 2);
-    //setKeyUsage(certificate, false, false, false, false, false, true, false);
-
-    var names = getSubjectAltNames();
-    if (names) {
-        setAltName(certificate, names);
-    }
-
-    //if (CACert.subject && CACert.privateKey) {
-    return createCertificateFinal(certificate, CACert.privateKey, "RSASSA-PKCS1-v1_5", "SHA-256").then(result =>
-    {
-        privateKey = result.privateKey;
-    }) .
-    then(() =>
-    {
-        var pemCert = convertBinaryToPem(certificate.toSchema(true).toBER(false), "CERTIFICATE");
-        document.getElementById("certificate-pem").textContent = pemCert;
-        var pemUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(pemCert);
-        document.getElementById("certificate-download").setAttribute("href", pemUrl);
-    }) .
-    then(() =>
-    {
-        crypto.exportKey("pkcs8", privateKey).
-        then(pkcs8 =>
-        {
-            var pemPrivateKey = convertBinaryToPem(pkcs8, "PRIVATE KEY");
-            document.getElementById("private-key-pem").textContent = pemPrivateKey;
-            var pemUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(pemPrivateKey);
-            document.getElementById("private-key-download").setAttribute("href", pemUrl);
-        });
-    });
-*/
 }
-
-
-// Returns a Promise yielding the certificate object
-function buildCertificateObject(subject, keyPair, issuer, issuerPrivateKey) {
-    var cert = new org.pkijs.simpl.CERT();
-
-    cert.version = 2;
-    setSerialNumber(cert, Date.now());
-    setEntity2(cert.subject, subject);
-    setEntity2(cert.issuer, issuer)
-
-    setValidityPeriod(cert, new Date(), 365);  // Good from today for 730 days
-    setEmptyExtensions(cert);
-
-    var names = getSubjectAltNames();
-    if (names) {
-        setAltName(cert, names);
-    }
-    //setAltName(cert);
-    setCABit(cert, false);
-    //setKeyUsage(cert, true, true, true, false, false, false, false); // digitalSignature, nonRepudiation, keyCertSign, cRLSign
-    //setExtendKeyUsage(cert);
-    setSignatureAlgorithm(cert, "1.2.840.113549.1.1.11"); // RSA with SHA-256
-
-    return setPublicKey(cert, keyPair.publicKey).
-        then(function() {return signCert(cert, "1.2.840.113549.1.1.11", issuerPrivateKey)}).
-        then(function() {return cert});
-}
-// Helper functions
-
-function setSerialNumber(cert, serialNumber) {
-    cert.serialNumber = new asn1js.Integer({value: serialNumber});
-}
-
-function setValidityPeriod(cert, startDate, durationInDays) {
-    // Normalize to midnight
-    var start = new Date(startDate);
-    start.setHours(0);
-    start.setMinutes(0);
-    start.setSeconds(0);
-    var end   = new Date(start.getTime() + durationInDays * 24 * 60 * 60 * 1000);
-
-    cert.notBefore.value = start;
-    cert.notAfter.value  = end;
-}
-
-function setEmptyExtensions(cert) {
-    cert.extensions = new Array();
-}
-
-function setCABit(cert, isCA, pathlen) {
-    var value = {
-        cA: isCA,
-    }
-
-    if (isCA && pathlen) {
-        value.pathLenConstraint = pathlen;
-    }
-
-    var basicConstraints = new BasicConstraints(value);
-    cert.extensions.push(new Extension({
-        extnID: "2.5.29.19",
-        critical: true,
-        extnValue: basicConstraints.toSchema().toBER(false),
-        parsedValue: basicConstraints
-    }));
-}
-
-function setKeyUsage(cert, digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign, cRLSign) {
-    var keyUsageBits = new ArrayBuffer(1);
-    var keyUsageBytes = new Uint8Array(keyUsageBits);
-
-    keyUsageBytes[0] = 0;
-    if (digitalSignature)   {keyUsageBytes[0] |= 0x80;}
-    if (nonRepudiation)     {keyUsageBytes[0] |= 0x40;}
-    if (keyEncipherment)    {keyUsageBytes[0] |= 0x20;}
-    if (dataEncipherment)   {keyUsageBytes[0] |= 0x10;}
-    if (keyAgreement)       {keyUsageBytes[0] |= 0x08;}
-    if (keyCertSign)        {keyUsageBytes[0] |= 0x04;}
-    if (cRLSign)            {keyUsageBytes[0] |= 0x02;}
-
-    var keyUsage = new asn1js.BitString({valueHex: keyUsageBits});
-    cert.extensions.push(new Extension({
-        extnID: "2.5.29.15",
-        critical: false,
-        extnValue: keyUsage.toBER(false),
-        parsedValue: keyUsage
-    }));
-}
-
-function setAltName(cert, names) {
-    var generalNames = new Array();
-    for (var i = 0; i < names.length; i++) {
-        generalNames.push(new GeneralName({
-            type: 2,
-            value: names[i]
-        }));
-    }
-
-    var altNames = new GeneralNames({
-        names: generalNames
-    })
-    cert.extensions.push(new Extension({
-        extnID: "2.5.29.17",
-        extnValue: altNames.toSchema().toBER(false),
-    }));
-
-    return;
-}
-
-
-function setSignatureAlgorithm(cert, oid) {
-    cert.signatureAlgorithm.algorithm_id = oid; // In tbsCert
-}
-
-function setPublicKey(cert, publicKey) {
-    return cert.subjectPublicKeyInfo.importKey(publicKey);
-}
-
-function signCert(cert, oid, privateKey) {
-    cert.signature.algorithm_id = oid; // In actual signature
-    return cert.sign(privateKey);
-    //return cert.sign(CAPrivateKey);
-}
-
-
-// General helper functions
-
-function arrayBufferToBase64String(arrayBuffer) {
-    var byteArray = new Uint8Array(arrayBuffer)
-    var byteString = '';
-
-    for (var i=0; i<byteArray.byteLength; i++) {
-        byteString += String.fromCharCode(byteArray[i]);
-    }
-
-    return btoa(byteString);
-}
-
-
-
-function convertBinaryToPem(binaryData, label) {
-
-    var base64Cert = arrayBufferToBase64String(binaryData);
-
-    var pemCert = "-----BEGIN " + label + "-----\r\n";
-
-    var nextIndex = 0;
-    var lineLength;
-    while (nextIndex < base64Cert.length) {
-        if (nextIndex + 64 <= base64Cert.length) {
-            pemCert += base64Cert.substr(nextIndex, 64) + "\r\n";
-        } else {
-            pemCert += base64Cert.substr(nextIndex) + "\r\n";
-        }
-        nextIndex += 64;
-    }
-
-    pemCert += "-----END " + label + "-----\r\n";
-    return pemCert;
-}
-
-function pemToDer(pem, label) {
-    var pemString = String.fromCharCode.apply(null, new Uint8Array(pem));
-	var isOpensslPrivateKey = false;
-
-    if (label == "certificate") {
-        var res = pemString.match(/-----BEGIN CERTIFICATE-----([a-zA-Z0-9+/=\r\n]*?)-----END CERTIFICATE-----/m);
-        pemString = res[1]
-    } else if (label == "privatekey") {
-        var res = pemString.match(/-----BEGIN RSA PRIVATE KEY-----([a-zA-Z0-9+/=\r\n]*?)-----END RSA PRIVATE KEY-----/m);
-        if (res) {
-            isOpensslPrivateKey = true;
-            pemString = res[1];
-        } else {
-            res = pemString.match(/-----BEGIN PRIVATE KEY-----([a-zA-Z0-9+/=\r\n]*?)-----END PRIVATE KEY-----/m);
-            pemString = res[1];
-        }
-    }
-
-    // base64 decode
-	pemString = pemString.replace(/\r\n/g, "");
-    var derString = atob(pemString);
-
-    // String to ArrayBuffer
-
-    var buf = new ArrayBuffer(derString.length);
-    var bufView = new Uint8Array(buf);
-    for (var i = 0, strLen=derString.length; i < strLen; i++) {
-        bufView[i] = derString.charCodeAt(i);
-    }
-
-    if (!isOpensslPrivateKey) {
-        return buf;
-    }
-
-    // Convert openssl format privatekey to pkcs#8 format
-    var pkcs8 = new org.pkijs.simpl.PKCS8();
-    pkcs8.version = 0;
-    pkcs8.privateKeyAlgorithm = new org.pkijs.simpl.ALGORITHM_IDENTIFIER({algorithm_id:"1.2.840.113549.1.1.1", algorithm_params: new org.pkijs.asn1.NULL()})
-    pkcs8.privateKey = new org.pkijs.asn1.OCTETSTRING({value_hex:buf});
-    var buf2 = pkcs8.toSchema(true).toBER(false);
-    return buf2
-}
-
-function extract_entity(entity) {
-    var subj = entity.toJSON();
-    var names = {
-        commonName          : "",
-        organizationName    : "",
-        organizationUnitName : "",
-        countryCode         : "",
-        stateOrProvinceName : "",
-        localityName        : ""
-    };
-    for (var i = 0; i < subj["typesAndValues"].length; i++) {
-        var ty = subj["typesAndValues"][i]["type"];
-        var val = subj["typesAndValues"][i]["value"]["value_block"]["value"];
-        switch (ty) {
-        case "2.5.4.3":
-            names["commonName"] = val;
-            break;
-        case "2.5.4.6":
-            names["countryCode"] = val;
-            break;
-        case "2.5.4.7":
-            names["localityName"] = val;
-            break;
-        case "2.5.4.8":
-            names["stateOrProvinceName"] = val;
-            break;
-        case "2.5.4.10":
-            names["organizationName"] = val;
-            break;
-        case "2.5.4.11":
-            names["organizationUnitName"] = val;
-            break;
-        default:
-            break;
-        }
-    }
-
-    return names;
-}
-
-let uploadCache = {};
 
 function handleOriginCert(evt) {
     var radio = document.getElementsByName("originformat");
@@ -616,20 +277,6 @@ function handleOriginCert(evt) {
             elem = document.getElementById("origin-jks");
             elem.style.display = "block"
         }
-    }
-}
-
-function setPEMContent(cert, pkey) {
-    if (typeof cert != "undefined") {
-        pemCertUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(cert)
-        document.getElementById("certificate-pem").textContent = cert
-        document.getElementById("certificate-download").setAttribute("href", pemCertUrl)
-    }
-
-    if (typeof pkey != "undefined") {
-        pemPkeyUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(pkey)
-        document.getElementById("private-key-pem").textContent = pkey
-        document.getElementById("private-key-download").setAttribute("href", pemPkeyUrl)
     }
 }
 
@@ -663,7 +310,6 @@ function handleUploadPKCS12Cert(input) {
     tempReader.onload =
         function(event)
         {
-
             var password = document.getElementById("pkcs12-password").value;
 
             var dst = new Uint8Array(event.target.result);
